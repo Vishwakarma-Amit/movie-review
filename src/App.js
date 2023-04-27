0c05b9e0df9f43c5c6fa62db4041dce659c84b4d
Amit Vishwakarma <radhey@192.168.1.2>
2023-04-27 15:22:12 +0530

    Movie-Review App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import api from './api/axiosConfig'
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      setMovies(response.data);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data
+      setMovie(singleMovie)
+      // setReviews(singleMovie.reviews)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home movies={movies} />} />
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer />} />
+          <Route path='/Reviews/:movieId' element={<Reviews getMovieData={getMovieData}
+            reviews={reviews} setReviews={setReviews} movie={movie} />} />
+        </Route>
+      </Routes>
+    </div >
   );
 }
 
